d7fe0283c42910347a21ae3082d83989c07ee4a3
Benjamin Willenbring <benjamin.willenbring@autodesk.com>
2020-05-21 18:06:39 -0400

    Update slack.js
    Slack message

--- scripts/slack.js
+++ scripts/slack.js
@@ -2,36 +2,36 @@
 const axios = require('axios');
 const sep = '-'.repeat(75);
 
-const notify = () => {
-    const url = process.env['slack_webhooks_url'];
-    // Compose the message, using Slack's block kit
-    const blocks = [
-        {
-            type: 'section',
-            text: {
-                type: 'mrkdwn',
-                text: ":successful: Message was a success!"
+module.exports = {
+    notify: () => {
+        const url = process.env['slack_webhooks_url'];
+        // Compose the message, using Slack's block kit
+        const blocks = [
+            {
+                type: 'section',
+                text: {
+                    type: 'mrkdwn',
+                    text: ":successful: Regresso message was a success!"
+                }
             }
+        ]
+        // Compose the message request
+        data = {
+            channel: '@willenb', 
+            blocks: blocks,
         }
-    ]
-    // Compose the message request
-    data = {
-        channel: '@willenb', 
-        blocks: blocks,
+
+        console.log(data);
+        // Send the slack message 
+        axios
+            .post(url, data)
+            .catch((err) => {
+                console.log(`Something went wrong! See error message below:\n${err.message}\n${sep}`);
+            })
+            .then(r => {
+                if (r.status == 200) {
+                    console.log('Slack message sent!');
+                }
+            });
     }
-
-    console.log(data);
-    // Send the slack message 
-    axios
-        .post(url, data)
-        .catch((err) => {
-            console.log(`Something went wrong! See error message below:\n${err.message}\n${sep}`);
-        })
-        .then(r => {
-            if (r.status == 200) {
-                console.log('Slack message sent!');
-            }
-        });
 }
-
-notify();
